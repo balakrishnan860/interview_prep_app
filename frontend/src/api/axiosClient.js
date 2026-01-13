10a8efe4c2d6b187dec3254fe55c9b77cfa24b0c
balakrishnan860 <rbk2072002@gmail.com>
2026-01-13 13:10:25 +0530

    baseUrl change

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+    baseURL: "https://interview-prep-app-9jbs.onrender.com/api",
     withCredentials: true
 });
+
+console.log("env_url:",import.meta.env.VITE_BACKEND_URL)
 
 export default axiosClient;
